fc066670ca7fef84547a69d921874dd2ea33afda
zusheng <imzusheng@163.com>
2021-12-22 18:45:31 +0800

    update: 详情页table、th、td宽度控制

--- src/plugins/hljsDirective.js
+++ src/plugins/hljsDirective.js
@@ -8,7 +8,7 @@ import hljs from 'highlight.js'
 // hljs.registerLanguage('shell', require('highlight.js/lib/languages/shell'))
 // hljs.registerLanguage('bash', require('highlight.js/lib/languages/bash'))
 // hljs.registerLanguage('html', require('highlight.js/lib/languages/xml'))
-
+// TODO markdown中左括号用&lt右括号;&gt;替换，不然会被转换成html
 const store = exposeStore()
 
 export default {
